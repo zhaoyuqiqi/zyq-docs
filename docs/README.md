27232dce8d278aa3ee682dd0acadac0d793412ee
zhaoyuqiqi <1940988349@qq.com>
2023-03-09 00:03:15 +0800

    feat: 新增部分页面

--- docs/README.md
+++ docs/README.md
@@ -1,1 +1,14 @@
-## hello World`
+---
+home: true
+heroImage: /images/header-logo.jpg
+heroText: zhao77
+tagline: zhao77学习路线与心得
+features:
+- title: 框架
+  details: 介绍Vue与React两大前端框架用法及其原理。
+- title: 算法
+  details: 介绍前端常用算法以及Vue、React底层实现使用到的算法。
+- title: 技巧
+  details: 介绍前端js中常用到的奇技淫巧
+footer: 你的前景远超你的想象
+---
--- vuepress.config.ts
+++ vuepress.config.ts
@@ -1,7 +1,70 @@
-import { defineUserConfig } from "vuepress";
+import { defineUserConfig, defaultTheme } from "vuepress";
 
 export default defineUserConfig({
   lang: "zh-CN",
-  title: "你好， VuePress ！",
-  description: "这是我的第一个 VuePress 站点",
+  title: "爱哭的赵一一",
+  description: "前端个人学习站点，总结个人学习历程与经验",
+  theme: defaultTheme({
+    navbar: [
+      { text: "主页", link: "/" },
+      {
+        text: "Vue",
+        link: "/vue/",
+      },
+      {
+        text: "React",
+        link: "/react/",
+      },
+      {
+        text: "奇技淫巧",
+        children: [
+          {
+            text: "Promise",
+            link: "/skill/promise/",
+          },
+          {
+            text: "杂项",
+            link: "/skill/messy/",
+          },
+        ],
+      },
+      {
+        text: "算法",
+        children: [
+          {
+            text: "二分法",
+            link: "/algorithm/binnary/",
+          },
+          {
+            text: "双指针",
+            link: "/algorithm/double-pointer/",
+          },
+          {
+            text: "动态规划",
+            link: "/algorithm/dp/",
+          },
+          {
+            text: "二叉树",
+            link: "/algorithm/binnary-tree/",
+          },
+          {
+            text: "排序算法",
+            link: "/algorithm/sort/",
+          },
+        ],
+      },
+      { text: "B站主页", link: "https://space.bilibili.com/279282087" },
+      { text: "掘金主页", link: "https://juejin.cn/user/3447712806348557/posts" },
+      // 下拉列表的配置
+      // {
+      //   text: "Languages",
+      //   children: [
+      //     { text: "Chinese", link: "/language/chinese" },
+      //     { text: "English", link: "/language/English" },
+      //   ],
+      // },
+    ],
+    logo: "/images/header-logo.jpg",
+  
+  }),
 });
